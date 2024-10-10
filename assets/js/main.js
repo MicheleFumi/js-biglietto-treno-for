8c7f6513a323c2437501207009abfc4323ee7145
MicheleFumi <michele.fumi96@gmail.com>
2024-10-10 09:36:22 +0200

    bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,39 +129,3 @@ cardEl.insertAdjacentHTML('beforeend', memberEl);
 
 
 
-// MILESTONE 2
-
-/* // FASE 1: CREIAMO I DUE DATI KM E ETA'
-let name_surname;
-let kilometer;
-let age;
-let ticket_price = 0.21;
-  
-//CALCOLIAMO SUBTOTAL
-let subtotal = kilometer * ticket_price ;
-
-//LOGGHIAMO KILOMETER, AGE E SUBTOTAL, 
-console.log(kilometer);
-console.log(age);
-console.log(subtotal);
-
-
-// FASE 2: CREARE LE CONDIZIONALI DI ETA'
-
-// CREIAMO DELLE VARIABILI DOVE SI CALCOLA LO SCONTO
-
-let junior = (subtotal * 20) / 100;
-let senior = (subtotal * 40) / 100;
-//LOGGHIAMO LE VARIABILI MENO LO SCONTO CALCOLATO PRIMA IN BASE ALLA CONDIZIONE NECESSARIA
-
-if (age < 18){
- console.log((subtotal - junior).toFixed(2));
- 
-    
-}else if (age > 65){
-    console.log((subtotal - senior).toFixed(2));
-    
-}else{
-    console.log(subtotal);
-    
-} */
